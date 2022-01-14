c5d873fae9c76fe14af431305cc9d77b382f848f
AyushmaanAggarwal <ayu02468@gmail.com>
2022-01-14 02:43:48 -0500

    Patched DWM

--- config.def.h
+++ config.def.h
@@ -74,7 +74,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-        { MODKEY,                       Shift_L,      spawndefault,   {0} },
+        { MODKEY,                       XK_s,      spawndefault,   {0} },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
