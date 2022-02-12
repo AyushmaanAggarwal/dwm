79692c93f56cdaf645e5acde1c6d587e431b09a8
AyushmaanAggarwal <ayu02468@gmail.com>
2022-02-11 19:53:35 -0800

    Setup minor changes to make default tags for applications

--- config.def.h
+++ config.def.h
@@ -35,8 +35,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Xournalpp",NULL, NULL,	      1 << 2,	    0,           -1 },
+	{ "zathura",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Atom",     NULL,       NULL,       1 << 4,       0,           -1 },
+
 };
 
 /* layout(s) */
